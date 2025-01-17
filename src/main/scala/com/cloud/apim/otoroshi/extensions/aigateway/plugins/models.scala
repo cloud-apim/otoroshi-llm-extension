cf5dd6fe357fb64256f756a6dd206212284e1f07
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2025-01-17 22:51:16 +0100

    fix models plugins

--- src/main/scala/com/cloud/apim/otoroshi/extensions/aigateway/plugins/models.scala
+++ src/main/scala/com/cloud/apim/otoroshi/extensions/aigateway/plugins/models.scala
@@ -38,41 +38,45 @@ class OpenAiCompatModels extends NgBackendCall {
 
   override def callBackend(ctx: NgbBackendCallContext, delegates: () => Future[Either[NgProxyEngineError, BackendCallResponse]])(implicit env: Env, ec: ExecutionContext, mat: Materializer): Future[Either[NgProxyEngineError, BackendCallResponse]] = {
     val config = ctx.cachedConfig(internalName)(AiPluginRefsConfig.format).getOrElse(AiPluginRefsConfig.default)
-    val provider: Option[AiProvider] = ctx.request.queryParam("provider").filter(v => config.refs.contains(v)).flatMap { r =>
-      env.adminExtensions.extension[AiExtension].flatMap(_.states.provider(r))
-    }.orElse(
-      config.refs.headOption.flatMap { r =>
-        env.adminExtensions.extension[AiExtension].flatMap(_.states.provider(r))
+    val ext = env.adminExtensions.extension[AiExtension].get
+    Source(config.refs.toList)
+      .map(ref => ext.states.provider(ref))
+      .collect {
+        case Some(provider) => provider
       }
-    )
-    provider match {
-      case None => Right(BackendCallResponse(NgPluginHttpResponse.fromResult(Results.InternalServerError(Json.obj("error" -> "provider not found"))), None)).vfuture
-      case Some(provider) => {
-        provider.getChatClient() match {
-          case None => Right(BackendCallResponse(NgPluginHttpResponse.fromResult(Results.InternalServerError(Json.obj("error" -> "client not found"))), None)).vfuture
-          case Some(client) => {
-            client.listModels().map {
-              case Left(err) => Right(BackendCallResponse(NgPluginHttpResponse.fromResult(Results.InternalServerError(Json.obj("error" -> "operation error", "details" -> err))), None))
-              case Right(list) => {
-                val now: Long = System.currentTimeMillis() / 1000
-                Right(BackendCallResponse(NgPluginHttpResponse.fromResult(
-                  Results.Ok(Json.obj(
-                    "object" -> "list",
-                    "data" -> JsArray(list.map(m => Json.obj(
-                      "id" -> m,
-                      "pid" -> s"${provider.slugName}/${m}",
-                      "object" -> "model",
-                      "created" -> now,
-                      "owned_by" -> "openai"
-                    )))
-                  ))
-                ), None))
-              }
-            }
+      .map(p => (p, p.getChatClient()))
+      .collect {
+        case (provider, Some(client)) => (provider, client)
+      }
+      .mapAsync(1) {
+        case (provider, client) => client.listModels().map(e => (provider, e))
+      }
+      .collect {
+        case (provider, Right(list)) => list.map { model =>
+          if (config.refs.size == 1) {
+            model
+          } else {
+            if (model.contains("/")) s"${provider.slugName}###${model}" else s"${provider.slugName}/${model}"
           }
+          //(model, combined)
         }
       }
-    }
+      .flatMapConcat(list => Source(list))
+      .runWith(Sink.seq)
+      .map { list =>
+        val now: Long = System.currentTimeMillis() / 1000
+        Right(BackendCallResponse(NgPluginHttpResponse.fromResult(
+          Results.Ok(Json.obj(
+            "object" -> "list",
+            "data" -> JsArray(list.map(m => Json.obj(
+              "id" -> m,
+              "object" -> "model",
+              "created" -> now,
+              "owned_by" -> "openai"
+            )))
+          ))
+        ), None))
+      }
   }
 }
 
@@ -115,11 +119,15 @@ class OpenAiCompatProvidersWithModels extends NgBackendCall {
       }
       .collect {
         case (provider, Right(list)) => {
-          println(s"got list: ${list}")
           list.map { model =>
+            val combined = if (config.refs.size == 1) {
+              model
+            } else {
+              if (model.contains("/")) s"${provider.slugName}###${model}" else s"${provider.slugName}/${model}"
+            }
             Json.obj(
-              "id" -> model,
-              "combined_id" -> (if (model.contains("/")) s"${provider.slugName}###${model}" else s"${provider.slugName}/${model}"),
+              "id" -> combined,
+              "simple_id" -> model,
               "object" -> "model",
               "created" -> now,
               "owned_by" -> provider.computedName,
@@ -150,10 +158,10 @@ class LlmProviderModels extends NgBackendCall {
   override def categories: Seq[NgPluginCategory] = Seq(NgPluginCategory.Custom("Cloud APIM"), NgPluginCategory.Custom("AI - LLM"))
   override def steps: Seq[NgStep] = Seq(NgStep.CallBackend)
   override def useDelegates: Boolean = false
-  override def defaultConfigObject: Option[NgPluginConfig] = Some(AiPluginRefConfig.default)
+  override def defaultConfigObject: Option[NgPluginConfig] = Some(AiPluginRefsConfig.default)
   override def noJsForm: Boolean = true
-  override def configFlow: Seq[String] = AiPluginRefConfig.configFlow
-  override def configSchema: Option[JsObject] = AiPluginRefConfig.configSchema("LLM provider", "providers")
+  override def configFlow: Seq[String] = AiPluginRefsConfig.configFlow
+  override def configSchema: Option[JsObject] = AiPluginRefsConfig.configSchema("LLM provider", "providers")
 
   override def start(env: Env): Future[Unit] = {
     env.adminExtensions.extension[AiExtension].foreach { ext =>
@@ -163,29 +171,38 @@ class LlmProviderModels extends NgBackendCall {
   }
 
   override def callBackend(ctx: NgbBackendCallContext, delegates: () => Future[Either[NgProxyEngineError, BackendCallResponse]])(implicit env: Env, ec: ExecutionContext, mat: Materializer): Future[Either[NgProxyEngineError, BackendCallResponse]] = {
-    val config = ctx.cachedConfig(internalName)(AiPluginRefConfig.format).getOrElse(AiPluginRefConfig.default)
-    val provider = env.adminExtensions.extension[AiExtension].flatMap(_.states.provider(config.ref))
-    provider match {
-      case None => Right(BackendCallResponse(NgPluginHttpResponse.fromResult(Results.InternalServerError(Json.obj("error" -> "provider not found"))), None)).vfuture
-      case Some(provider) => {
-        provider.getChatClient() match {
-          case None => Right(BackendCallResponse(NgPluginHttpResponse.fromResult(Results.InternalServerError(Json.obj("error" -> "client not found"))), None)).vfuture
-          case Some(client) => {
-            client.listModels().map {
-              case Left(err) => Right(BackendCallResponse(NgPluginHttpResponse.fromResult(Results.InternalServerError(Json.obj("error" -> "operation error", "details" -> err))), None))
-              case Right(list) => {
-                Right(BackendCallResponse(NgPluginHttpResponse.fromResult(
-                  Results.Ok(Json.obj(
-                    "models" -> JsArray(list.map(_.json)),
-                    "provider_models" -> JsArray(list.map(v => s"${provider.slugName}/${v}".json)),
-                  ))
-                ), None))
-              }
-            }
+    val config = ctx.cachedConfig(internalName)(AiPluginRefsConfig.format).getOrElse(AiPluginRefsConfig.default)
+    val ext = env.adminExtensions.extension[AiExtension].get
+    Source(config.refs.toList)
+      .map(ref => ext.states.provider(ref))
+      .collect {
+        case Some(provider) => provider
+      }
+      .map(p => (p, p.getChatClient()))
+      .collect {
+        case (provider, Some(client)) => (provider, client)
+      }
+      .mapAsync(1) {
+        case (provider, client) => client.listModels().map(e => (provider, e))
+      }
+      .collect {
+        case (provider, Right(list)) => list.map { model =>
+          if (config.refs.size == 1) {
+            model
+          } else {
+            if (model.contains("/")) s"${provider.slugName}###${model}" else s"${provider.slugName}/${model}"
           }
         }
       }
-    }
+      .flatMapConcat(list => Source(list))
+      .runWith(Sink.seq)
+      .map { list =>
+        Right(BackendCallResponse(NgPluginHttpResponse.fromResult(
+          Results.Ok(Json.obj(
+            "models" -> JsArray(list.map(_.json)),
+          ))
+        ), None))
+      }
   }
 }
 
@@ -226,7 +243,13 @@ class LlmProvidersWithModels extends NgBackendCall {
         case (provider, client) => client.listModels().map(e => (provider, e))
       }
       .collect {
-        case (provider, Right(list)) => list.map(model => (if (model.contains("/")) s"${provider.slugName}###${model}" else s"${provider.slugName}/${model}"))
+        case (provider, Right(list)) => list.map { model =>
+          if (config.refs.size == 1) {
+            model
+          } else {
+            if (model.contains("/")) s"${provider.slugName}###${model}" else s"${provider.slugName}/${model}"
+          }
+        }
       }
       .flatMapConcat(list => Source(list))
       .runWith(Sink.seq)
