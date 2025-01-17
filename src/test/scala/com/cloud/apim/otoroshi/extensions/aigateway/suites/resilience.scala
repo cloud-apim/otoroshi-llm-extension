8c22e0b7ec353005631564669d280956e4f4d3c6
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2025-01-17 15:29:22 +0100

    add resilience test suite

--- src/test/scala/com/cloud/apim/otoroshi/extensions/aigateway/suites/resilience.scala
+++ src/test/scala/com/cloud/apim/otoroshi/extensions/aigateway/suites/resilience.scala
@@ -1,5 +1,173 @@
 package com.cloud.apim.otoroshi.extensions.aigateway.suites
 
-class resilience {
+import com.cloud.apim.otoroshi.extensions.aigateway.LlmExtensionOneOtoroshiServerPerSuite
+import com.cloud.apim.otoroshi.extensions.aigateway.entities.AiProvider
+import otoroshi.models.EntityLocation
+import otoroshi.next.models._
+import otoroshi.utils.syntax.implicits.BetterFuture
+import otoroshi_plugins.com.cloud.apim.otoroshi.extensions.aigateway.plugins.OpenAiCompatProxy
+import play.api.libs.json.Json
+import reactor.core.publisher.Mono
+
+import java.util.UUID
+import scala.concurrent.duration.DurationInt
+
+class ResilienceSuite extends LlmExtensionOneOtoroshiServerPerSuite {
+
+  val (ollama1Port, _) = createTestServerWithRoutes("ollama1", routes => routes.post("/api/chat", (req, response) => {
+    req.receiveContent().ignoreElements().subscribe()
+    response
+      .status(200)
+      .addHeader("Content-Type", "application/json")
+      .sendString(Mono.just(
+        s"""{
+           |  "model": "foo",
+           |  "created_at": "2023-12-12T14:13:43.416799Z",
+           |  "message": {
+           |    "role": "assistant",
+           |    "content": "I am the ollama1 with model foo"
+           |  },
+           |  "done": true,
+           |  "total_duration": 5191566416,
+           |  "load_duration": 2154458,
+           |  "prompt_eval_count": 26,
+           |  "prompt_eval_duration": 383809000,
+           |  "eval_count": 298,
+           |  "eval_duration": 4799921000
+           |}""".stripMargin))
+  }))
+
+  val (ollama2Port, _) = createTestServerWithRoutes("ollama2", routes => routes.post("/api/chat", (req, response) => {
+    req.receiveContent().ignoreElements().subscribe()
+    response
+      .status(400)
+      .addHeader("Content-Type", "application/json")
+      .sendString(Mono.just(
+        s"""{
+           |  "error": "bad_request"
+           |}""".stripMargin))
+  }))
+
+  def ollamaProvider(name: String, model: String, port: Int, fallback: Option[String]): AiProvider = {
+    AiProvider(
+      id = UUID.randomUUID().toString,
+      name = name,
+      provider = "ollama",
+      connection = Json.obj(
+        "base_url" -> s"http://localhost:${port}",
+        "timeout" -> 30000
+      ),
+      options = Json.obj(
+        "model" -> model,
+      ),
+      providerFallback = fallback
+    )
+  }
+
+  test("llm provider will fallback on bad response") {
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////                                  setup                                                         ///////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    val llmProvider1 = ollamaProvider("ollama 1", "llama3.2", ollama1Port, None)
+    val llmProvider2 = ollamaProvider("ollama 2", "llama3.2", ollama2Port, Some(llmProvider1.id))
+
+    val route = NgRoute(
+      location = EntityLocation.default,
+      id = UUID.randomUUID().toString,
+      name = "test route",
+      description = "test route",
+      tags = Seq.empty,
+      metadata = Map.empty,
+      enabled = true,
+      debugFlow = false,
+      capture = false,
+      exportReporting = false,
+      frontend = NgFrontend.empty.copy(domains = Seq(NgDomainAndPath("test.oto.tools/chat"))),
+      backend = NgBackend.empty.copy(targets = Seq(NgTarget.default)),
+      plugins = NgPlugins(Seq(NgPluginInstance(
+        plugin = s"cp:${classOf[OpenAiCompatProxy].getName}",
+        config = NgPluginInstanceConfig(Json.obj(
+          "refs" -> Json.arr(llmProvider2.id)
+        ))
+      )))
+    )
+    client.forEntity("ai-gateway.extensions.cloud-apim.com", "v1", "providers").upsertEntity(llmProvider1).awaitf(10.seconds)
+    client.forEntity("ai-gateway.extensions.cloud-apim.com", "v1", "providers").upsertEntity(llmProvider2).awaitf(10.seconds)
+    client.forEntity("proxy.otoroshi.io", "v1", "routes").upsertEntity(route).awaitf(10.seconds)
+    await(2.seconds)
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////                                  test                                                          ///////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    val res = client.call("POST", s"http://test.oto.tools:${port}/chat", Map.empty, Some(Json.obj(
+      "messages" -> Json.arr(Json.obj(
+        "role" -> "user",
+        "content" -> "whatever"
+      ))
+    ))).awaitf(30.seconds)
+
+    assertEquals(res.status, 200, "status should be 200")
+    assert(res.body.contains("I am the ollama1"), "body contains I am the ollama1")
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////                                  teardown                                                      ///////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    client.forEntity("ai-gateway.extensions.cloud-apim.com", "v1", "providers").deleteEntity(llmProvider1).awaitf(10.seconds)
+    client.forEntity("ai-gateway.extensions.cloud-apim.com", "v1", "providers").deleteEntity(llmProvider2).awaitf(10.seconds)
+    client.forEntity("proxy.otoroshi.io", "v1", "routes").deleteEntity(route).awaitf(10.seconds)
+    await(2.seconds)
+  }
+
+  test("llm provider will fallback on network error") {
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////                                  setup                                                         ///////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    val llmProvider1 = ollamaProvider("ollama 1", "llama3.2", ollama1Port, None)
+    val llmProvider2 = ollamaProvider("ollama 2", "llama3.2", freePort, Some(llmProvider1.id))
+
+    val route = NgRoute(
+      location = EntityLocation.default,
+      id = UUID.randomUUID().toString,
+      name = "test route",
+      description = "test route",
+      tags = Seq.empty,
+      metadata = Map.empty,
+      enabled = true,
+      debugFlow = false,
+      capture = false,
+      exportReporting = false,
+      frontend = NgFrontend.empty.copy(domains = Seq(NgDomainAndPath("test.oto.tools/chat"))),
+      backend = NgBackend.empty.copy(targets = Seq(NgTarget.default)),
+      plugins = NgPlugins(Seq(NgPluginInstance(
+        plugin = s"cp:${classOf[OpenAiCompatProxy].getName}",
+        config = NgPluginInstanceConfig(Json.obj(
+          "refs" -> Json.arr(llmProvider2.id)
+        ))
+      )))
+    )
+    client.forEntity("ai-gateway.extensions.cloud-apim.com", "v1", "providers").upsertEntity(llmProvider1).awaitf(10.seconds)
+    client.forEntity("ai-gateway.extensions.cloud-apim.com", "v1", "providers").upsertEntity(llmProvider2).awaitf(10.seconds)
+    client.forEntity("proxy.otoroshi.io", "v1", "routes").upsertEntity(route).awaitf(10.seconds)
+    await(2.seconds)
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////                                  test                                                          ///////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+
+    val res = client.call("POST", s"http://test.oto.tools:${port}/chat", Map.empty, Some(Json.obj(
+      "messages" -> Json.arr(Json.obj(
+        "role" -> "user",
+        "content" -> "whatever"
+      ))
+    ))).awaitf(30.seconds)
+
+    assertEquals(res.status, 200, "status should be 200")
+    assert(res.body.contains("I am the ollama1"), "body contains I am the ollama1")
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    /////////                                  teardown                                                      ///////////
+    ////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
+    client.forEntity("ai-gateway.extensions.cloud-apim.com", "v1", "providers").deleteEntity(llmProvider1).awaitf(10.seconds)
+    client.forEntity("ai-gateway.extensions.cloud-apim.com", "v1", "providers").deleteEntity(llmProvider2).awaitf(10.seconds)
+    client.forEntity("proxy.otoroshi.io", "v1", "routes").deleteEntity(route).awaitf(10.seconds)
+    await(2.seconds)
+  }
 
 }
