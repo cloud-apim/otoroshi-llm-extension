3cc96fff99e58e3a8764de7b0b900ac0ee4ca190
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2026-03-26 10:27:22 +0100

    introducing an openai api aggregator plugin

--- src/main/scala/com/cloud/apim/otoroshi/extensions/aigateway/plugins/openaiproxy.scala
+++ src/main/scala/com/cloud/apim/otoroshi/extensions/aigateway/plugins/openaiproxy.scala
@@ -16,6 +16,92 @@ import play.api.mvc.Results
 
 import scala.concurrent.{ExecutionContext, Future}
 
+object OpenAiCompatProxy {
+  def validate(messages: Seq[JsObject], ctx: NgbBackendCallContext): Boolean = {
+    ctx.attrs.get(AiPluginsKeys.PromptValidatorsKey) match {
+      case None => true
+      case Some(seq) => {
+        val contents = messages.map(_.select("content").asOpt[String].getOrElse(""))
+        contents.forall(content => seq.forall(_.validate(content)))
+      }
+    }
+  }
+
+  def call(_jsonBody: JsValue, config: AiPluginRefsConfig, ctx: NgbBackendCallContext)(implicit ec: ExecutionContext, env: Env): Future[Either[NgProxyEngineError, BackendCallResponse]] = {
+    val jsonBody: JsValue = AiPluginRefsConfig.extractProviderFromModelInBody(_jsonBody, config)
+    val provider: Option[AiProvider] = jsonBody.select("provider").asOpt[String].filter(v => config.refs.contains(v)).flatMap { r =>
+      env.adminExtensions.extension[AiExtension].flatMap(_.states.provider(r))
+    }.orElse(
+      config.refs.headOption.flatMap { r =>
+        env.adminExtensions.extension[AiExtension].flatMap(_.states.provider(r))
+      }
+    )
+    provider.flatMap(_.getChatClient()) match {
+      case None => Left(NgProxyEngineError.NgResultProxyEngineError(Results.InternalServerError(Json.obj("error" -> "provider not found")))).vfuture
+      case Some(client) => {
+        val stream = ctx.request.queryParam("stream").contains("true") || ctx.request.header("x-stream").contains("true") || jsonBody.select("stream").asOpt[Boolean].contains(true)
+        val requestMessages = ctx.attrs.get(AiPluginsKeys.PromptTemplateKey) match {
+          case None => jsonBody.select("messages").asOpt[Seq[JsObject]].getOrElse(Seq.empty)
+          case Some(template) => {
+            val context = Json.obj(
+              "request" -> (ctx.json.asObject ++ Json.obj(
+                "route"              -> ctx.route.json,
+                "request"            -> ctx.request.json
+              )),
+              "body" -> jsonBody,
+              "properties" -> Json.obj(),
+            )
+            AiLlmProxy.applyTemplate(template, context)
+          }
+        }
+
+        if (validate(requestMessages, ctx)) {
+          val (preContextMessages, postContextMessages) = ctx.attrs.get(AiPluginsKeys.PromptContextKey).getOrElse((Seq.empty, Seq.empty))
+          val messages = (preContextMessages ++ requestMessages ++ postContextMessages).map { obj =>
+            InputChatMessage.fromJson(obj)
+          }
+          if (stream) {
+            client.tryStream(ChatPrompt(messages), ctx.attrs, jsonBody).map {
+              case Left(err) => Left(NgProxyEngineError.NgResultProxyEngineError(Results.BadRequest(err)))
+              case Right(source) => {
+                val finalSource = source
+                  .map(_.openaiEventSource(env))
+                  .concat(Source.single("data: [DONE]\n\n".byteString))
+                Right(BackendCallResponse(NgPluginHttpResponse.fromResult(Results.Ok.chunked(finalSource).as("text/event-stream")), None))
+              }
+            }
+          } else {
+            client.call(ChatPrompt(messages), ctx.attrs, jsonBody).map {
+              case Left(err) => Left(NgProxyEngineError.NgResultProxyEngineError(Results.BadRequest(err)))
+              case Right(response) => Right(BackendCallResponse(NgPluginHttpResponse.fromResult(Results.Ok(response.openaiJson(client.computeModel(jsonBody).getOrElse("none"), env))
+                .withHeaders(response.metadata.cacheHeaders.toSeq: _*)), None))
+            }
+          }
+        } else {
+          Left(NgProxyEngineError.NgResultProxyEngineError(Results.BadRequest(Json.obj("error" -> "bad_request", "error_description" -> "invalid request")))).vfuture
+        }
+      }
+    }
+  }
+
+  def handleRequest(config: AiPluginRefsConfig, ctx: NgbBackendCallContext)(implicit env: Env, ec: ExecutionContext, mat: Materializer): Future[Either[NgProxyEngineError, BackendCallResponse]] = {
+    if (ctx.request.hasBody) {
+      ctx.request.body.runFold(ByteString.empty)(_ ++ _).flatMap { bodyRaw =>
+        try {
+          val jsonBody = bodyRaw.utf8String.parseJson
+          call(jsonBody, config, ctx)
+        } catch {
+          case e: Throwable =>
+            e.printStackTrace()
+            NgProxyEngineError.NgResultProxyEngineError(Results.BadRequest(Json.obj("error" -> "bad_request", "error_details" -> e.getMessage))).leftf
+        }
+      }
+    } else {
+      call(Json.obj(), config, ctx)
+    }
+  }
+}
+
 class OpenAiCompatProxy extends NgBackendCall {
 
   override def name: String = "Cloud APIM - LLM OpenAI chat/completions Proxy"
@@ -39,100 +125,9 @@ class OpenAiCompatProxy extends NgBackendCall {
     ().vfuture
   }
 
-  def call(_jsonBody: JsValue, config: AiPluginRefsConfig, ctx: NgbBackendCallContext)(implicit ec: ExecutionContext, env: Env): Future[Either[NgProxyEngineError, BackendCallResponse]] = {
-    // println(s"\n\nreq: ${ctx.request.json.prettify}\n\n")
-    // println(s"\n\nctx: ${Json.obj(
-    //   "user" -> ctx.user.map(_.json).getOrElse(JsNull).asValue,
-    //   "apikey" -> ctx.apikey.map(_.json).getOrElse(JsNull).asValue,
-    // ).prettify}\n\n")
-    // println(s"\n\nbody: ${jsonBody.prettify}\n\n")
-    val jsonBody: JsValue = AiPluginRefsConfig.extractProviderFromModelInBody(_jsonBody, config)
-    val provider: Option[AiProvider] = jsonBody.select("provider").asOpt[String].filter(v => config.refs.contains(v)).flatMap { r =>
-      env.adminExtensions.extension[AiExtension].flatMap(_.states.provider(r))
-    }.orElse(
-      config.refs.headOption.flatMap { r =>
-        env.adminExtensions.extension[AiExtension].flatMap(_.states.provider(r))
-      }
-    )
-    provider.flatMap(_.getChatClient()) match {
-      case None => Left(NgProxyEngineError.NgResultProxyEngineError(Results.InternalServerError(Json.obj("error" -> "provider not found")))).vfuture // TODO: rewrite error
-      case Some(client) => {
-        val stream = ctx.request.queryParam("stream").contains("true") || ctx.request.header("x-stream").contains("true") || jsonBody.select("stream").asOpt[Boolean].contains(true)
-        val requestMessages = ctx.attrs.get(AiPluginsKeys.PromptTemplateKey) match {
-          case None => jsonBody.select("messages").asOpt[Seq[JsObject]].getOrElse(Seq.empty)
-          case Some(template) => {
-            val context = Json.obj(
-              "request" -> (ctx.json.asObject ++ Json.obj(
-                "route"              -> ctx.route.json,
-                "request"            -> ctx.request.json
-              )),
-              "body" -> jsonBody,
-              "properties" -> Json.obj(), // TODO: from conf
-            )
-            AiLlmProxy.applyTemplate(template, context)
-          }
-        }
-
-        if (validate(requestMessages, ctx)) {
-          val (preContextMessages, postContextMessages) = ctx.attrs.get(AiPluginsKeys.PromptContextKey).getOrElse((Seq.empty, Seq.empty))
-          val messages = (preContextMessages ++ requestMessages ++ postContextMessages).map { obj =>
-            // val role = obj.select("role").asOpt[String].getOrElse("user")
-            // val content = obj.select("content").asOpt[String].getOrElse("")
-            // val prefix = obj.select("prefix").asOptBoolean
-            // ChatMessage.input(role, content, prefix)
-            InputChatMessage.fromJson(obj)
-          }
-          if (stream) {
-            client.tryStream(ChatPrompt(messages), ctx.attrs, jsonBody).map {
-              case Left(err) => Left(NgProxyEngineError.NgResultProxyEngineError(Results.BadRequest(err)))
-              case Right(source) => {
-                val finalSource = source
-                  .map(_.openaiEventSource(env))
-                  .concat(Source.single("data: [DONE]\n\n".byteString))
-                Right(BackendCallResponse(NgPluginHttpResponse.fromResult(Results.Ok.chunked(finalSource).as("text/event-stream")), None))
-              }
-            }
-          } else {
-            client.call(ChatPrompt(messages), ctx.attrs, jsonBody).map {
-              case Left(err) => Left(NgProxyEngineError.NgResultProxyEngineError(Results.BadRequest(err)))
-              case Right(response) => Right(BackendCallResponse(NgPluginHttpResponse.fromResult(Results.Ok(response.openaiJson(client.computeModel(jsonBody).getOrElse("none"), env))
-                .withHeaders(response.metadata.cacheHeaders.toSeq: _*)), None))
-            }
-          }
-        } else {
-          Left(NgProxyEngineError.NgResultProxyEngineError(Results.BadRequest(Json.obj("error" -> "bad_request", "error_description" -> "invalid request")))).vfuture // TODO: rewrite error
-        }
-      }
-    }
-  }
-
-  def validate(messages: Seq[JsObject], ctx: NgbBackendCallContext): Boolean = {
-    ctx.attrs.get(AiPluginsKeys.PromptValidatorsKey) match {
-      case None => true
-      case Some(seq) => {
-        val contents = messages.map(_.select("content").asOpt[String].getOrElse(""))
-        contents.forall(content => seq.forall(_.validate(content)))
-      }
-    }
-  }
-
   override def callBackend(ctx: NgbBackendCallContext, delegates: () => Future[Either[NgProxyEngineError, BackendCallResponse]])(implicit env: Env, ec: ExecutionContext, mat: Materializer): Future[Either[NgProxyEngineError, BackendCallResponse]] = {
-    if (ctx.request.hasBody) {
-      ctx.request.body.runFold(ByteString.empty)(_ ++ _).flatMap { bodyRaw =>
-        try {
-          val jsonBody = bodyRaw.utf8String.parseJson
-          val config = ctx.cachedConfig(internalName)(AiPluginRefsConfig.format).getOrElse(AiPluginRefsConfig.default)
-          call(jsonBody, config, ctx)
-        } catch {
-          case e: Throwable =>
-            e.printStackTrace()
-            NgProxyEngineError.NgResultProxyEngineError(Results.BadRequest(Json.obj("error" -> "bad_request", "error_details" -> e.getMessage))).leftf
-        }
-      }
-    } else {
-      val config = ctx.cachedConfig(internalName)(AiPluginRefsConfig.format).getOrElse(AiPluginRefsConfig.default)
-      call(Json.obj(), config, ctx)
-    }
+    val config = ctx.cachedConfig(internalName)(AiPluginRefsConfig.format).getOrElse(AiPluginRefsConfig.default)
+    OpenAiCompatProxy.handleRequest(config, ctx)
   }
 }
 
