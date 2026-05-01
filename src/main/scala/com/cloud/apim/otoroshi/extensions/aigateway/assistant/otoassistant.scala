44e97bf89e78bafed42da01baf1f5eb01aefc847
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2026-05-01 14:38:33 +0200

    ground work for the AI assistant in otoassistant.scala

--- src/main/scala/com/cloud/apim/otoroshi/extensions/aigateway/assistant/otoassistant.scala
+++ src/main/scala/com/cloud/apim/otoroshi/extensions/aigateway/assistant/otoassistant.scala
@@ -1,5 +1,29 @@
 package com.cloud.apim.otoroshi.extensions.aigateway.assistant
 
-class otoassistant {
+import akka.stream.scaladsl.Source
+import akka.util.ByteString
+import otoroshi.env.Env
+import otoroshi.models.{ApiKey, BackOfficeUser}
+import otoroshi.next.extensions._
+import otoroshi.utils.syntax.implicits._
+import otoroshi_plugins.com.cloud.apim.extensions.aigateway.AiExtension
+import play.api.libs.json._
+import play.api.mvc._
 
+import scala.concurrent.Future
+
+class OtoroshiAssistant(env: Env, ext: AiExtension) {
+
+  def isEnabled: Boolean = ext.states.allProviders().exists(_.isOtoroshiAssistant)
+
+  def handleAssistantCompletion(ctx: AdminExtensionRouterContext[AdminExtensionBackofficeAuthRoute], req: RequestHeader, user: Option[BackOfficeUser], body: Option[Source[ByteString, _]]): Future[Result] = {
+    implicit val ec = env.otoroshiExecutionContext
+    implicit val mat = env.otoroshiMaterializer
+    implicit val ev = env
+    if (isEnabled) {
+      ???
+    } else {
+      Results.InternalServerError(Json.obj("error" -> "Access is disabled")).vfuture
+    }
+  }
 }
