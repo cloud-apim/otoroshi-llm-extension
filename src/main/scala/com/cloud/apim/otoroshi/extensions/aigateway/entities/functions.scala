f215c07d4c7bd828761067cb27b7ea2d74b640b2
Mathieu Ancelin <mathieu.ancelin@gmail.com>
2026-06-24 15:32:53 +0200

    phase 3 of A2A support - #179 in functions.scala

--- src/main/scala/com/cloud/apim/otoroshi/extensions/aigateway/entities/functions.scala
+++ src/main/scala/com/cloud/apim/otoroshi/extensions/aigateway/entities/functions.scala
@@ -20,90 +20,102 @@ object LlmFunctions {
   def callToolsOpenai(functions: Seq[GenericApiResponseChoiceMessageToolCall], conns: Seq[String], providerName: String, attrs: TypedMap, nameToFunction: Map[String, String])(implicit ec: ExecutionContext, env: Env): Future[Seq[JsValue]] = {
     val wasmFunctions = functions.filter(_.isWasm)
     val searchFunctions = functions.filter(_.isSearchEngine)
-    val mcpConnectors = functions.filterNot(f => f.isWasm || f.isSearchEngine)
+    val a2aConnectors = functions.filter(_.isA2A)
+    val mcpConnectors = functions.filterNot(f => f.isWasm || f.isSearchEngine || f.isA2A)
     val inlineFunctionsF = LlmToolFunction._callInlineToolsOpenai(wasmFunctions.filter(_.isInline), providerName, attrs)(ec, env)
     val wasmFunctionsF = LlmToolFunction._callToolsOpenai(wasmFunctions.filterNot(_.isInline), providerName, attrs, nameToFunction)(ec, env)
     val mcpConnectorsF = McpSupport.callToolsOpenai(mcpConnectors, conns, providerName, attrs)(ec, env)
+    val a2aConnectorsF = A2ASupport.callToolsOpenai(a2aConnectors, providerName, attrs)(ec, env)
     val searchFunctionsF = SearchEngineSupport.callToolsOpenai(searchFunctions, providerName, attrs)(ec, env)
     for {
       wasmFunctionsR <- wasmFunctionsF
       mcpConnectorsR <- mcpConnectorsF
+      a2aConnectorsR <- a2aConnectorsF
       inlineFunctionsR <- inlineFunctionsF
       searchFunctionsR <- searchFunctionsF
-    } yield inlineFunctionsR ++ wasmFunctionsR ++ mcpConnectorsR ++ searchFunctionsR
+    } yield inlineFunctionsR ++ wasmFunctionsR ++ mcpConnectorsR ++ a2aConnectorsR ++ searchFunctionsR
   }
 
   def callToolsOllama(functions: Seq[GenericApiResponseChoiceMessageToolCall], conns: Seq[String], attrs: TypedMap, nameToFunction: Map[String, String])(implicit ec: ExecutionContext, env: Env): Future[Seq[JsValue]] = {
     val wasmFunctions = functions.filter(_.isWasm)
     val searchFunctions = functions.filter(_.isSearchEngine)
-    val mcpConnectors = functions.filterNot(f => f.isWasm || f.isSearchEngine)
+    val a2aConnectors = functions.filter(_.isA2A)
+    val mcpConnectors = functions.filterNot(f => f.isWasm || f.isSearchEngine || f.isA2A)
     val inlineFunctionsF = LlmToolFunction._callInlineToolsOllama(wasmFunctions.filter(_.isInline), attrs)(ec, env)
     val wasmFunctionsF = LlmToolFunction._callToolsOllama(wasmFunctions, attrs, nameToFunction)(ec, env)
     val mcpConnectorsF = McpSupport.callToolsOllama(mcpConnectors, conns, attrs)(ec, env)
+    val a2aConnectorsF = A2ASupport.callToolsOllama(a2aConnectors, attrs)(ec, env)
     val searchFunctionsF = SearchEngineSupport.callToolsOllama(searchFunctions, attrs)(ec, env)
     for {
       wasmFunctionsR <- wasmFunctionsF
       mcpConnectorsR <- mcpConnectorsF
+      a2aConnectorsR <- a2aConnectorsF
       inlineFunctionsR <- inlineFunctionsF
       searchFunctionsR <- searchFunctionsF
-    } yield inlineFunctionsR ++ wasmFunctionsR ++ mcpConnectorsR ++ searchFunctionsR
+    } yield inlineFunctionsR ++ wasmFunctionsR ++ mcpConnectorsR ++ a2aConnectorsR ++ searchFunctionsR
   }
 
   def callToolsAnthropic(functions: Seq[AnthropicApiResponseChoiceMessageToolCall], conns: Seq[String], providerName: String, attrs: TypedMap, nameToFunction: Map[String, String])(implicit ec: ExecutionContext, env: Env): Future[Seq[JsValue]] = {
     val wasmFunctions = functions.filter(_.isWasm)
     val searchFunctions = functions.filter(_.isSearchEngine)
-    val mcpConnectors = functions.filterNot(f => f.isWasm || f.isSearchEngine)
+    val a2aConnectors = functions.filter(_.isA2A)
+    val mcpConnectors = functions.filterNot(f => f.isWasm || f.isSearchEngine || f.isA2A)
     val inlineFunctionsF = LlmToolFunction._callInlineToolsAnthropic(wasmFunctions.filter(_.isInline), providerName, attrs)(ec, env)
     val wasmFunctionsF = LlmToolFunction._callToolsAnthropic(wasmFunctions, providerName, attrs, nameToFunction)(ec, env)
     val mcpConnectorsF = McpSupport.callToolsAnthropic(mcpConnectors, conns, providerName, attrs)(ec, env)
+    val a2aConnectorsF = A2ASupport.callToolsAnthropic(a2aConnectors, attrs)(ec, env)
     val searchFunctionsF = SearchEngineSupport.callToolsAnthropic(searchFunctions, providerName, attrs)(ec, env)
     for {
       wasmFunctionsR <- wasmFunctionsF
       mcpConnectorsR <- mcpConnectorsF
+      a2aConnectorsR <- a2aConnectorsF
       inlineFunctionsR <- inlineFunctionsF
       searchFunctionsR <- searchFunctionsF
-    } yield inlineFunctionsR ++ wasmFunctionsR ++ mcpConnectorsR ++ searchFunctionsR
+    } yield inlineFunctionsR ++ wasmFunctionsR ++ mcpConnectorsR ++ a2aConnectorsR ++ searchFunctionsR
   }
 
   def callToolsCohere(functions: Seq[GenericApiResponseChoiceMessageToolCall], conns: Seq[String], providerName: String, fmap: Map[String, String], attrs: TypedMap, nameToFunction: Map[String, String])(implicit ec: ExecutionContext, env: Env): Future[Seq[JsValue]] = {
     val wasmFunctions = functions.filter(_.isWasm)
     val searchFunctions = functions.filter(_.isSearchEngine)
-    val mcpConnectors = functions.filterNot(f => f.isWasm || f.isSearchEngine)
+    val a2aConnectors = functions.filter(_.isA2A)
+    val mcpConnectors = functions.filterNot(f => f.isWasm || f.isSearchEngine || f.isA2A)
     val inlineFunctionsF = LlmToolFunction.callInlineToolsCohere(wasmFunctions.filter(_.isInline), providerName, fmap, attrs)(ec, env)
     val wasmFunctionsF = LlmToolFunction.callToolsCohere(wasmFunctions, providerName, fmap, attrs, nameToFunction)(ec, env)
     val mcpConnectorsF = McpSupport.callToolsCohere(mcpConnectors, conns, providerName, fmap, attrs)(ec, env)
+    val a2aConnectorsF = A2ASupport.callToolsCohere(a2aConnectors, fmap, providerName, attrs)(ec, env)
     val searchFunctionsF = SearchEngineSupport.callToolsCohere(searchFunctions, providerName, attrs)(ec, env)
     for {
       wasmFunctionsR <- wasmFunctionsF
       mcpConnectorsR <- mcpConnectorsF
+      a2aConnectorsR <- a2aConnectorsF
       inlineFunctionsR <- inlineFunctionsF
       searchFunctionsR <- searchFunctionsF
-    } yield inlineFunctionsR ++ wasmFunctionsR ++ mcpConnectorsR ++ searchFunctionsR
+    } yield inlineFunctionsR ++ wasmFunctionsR ++ mcpConnectorsR ++ a2aConnectorsR ++ searchFunctionsR
   }
 
   def toolsWithInline(wasmFunctions: Seq[String], inlineFunctions: Seq[String], mcpConnectors: Seq[String], includeFunctions: Seq[String], excludeFunctions: Seq[String], attrs: TypedMap, searchEngines: Seq[String] = Seq.empty)(implicit ec: ExecutionContext, env: Env): JsObject = {
-    val tools: Seq[JsObject] = LlmToolFunction._tools(wasmFunctions) ++ LlmToolFunction._inlineTools(inlineFunctions, attrs) ++ McpSupport.tools(mcpConnectors, includeFunctions, excludeFunctions, attrs) ++ SearchEngineSupport.tools(searchEngines)
+    val tools: Seq[JsObject] = LlmToolFunction._tools(wasmFunctions) ++ LlmToolFunction._inlineTools(inlineFunctions, attrs) ++ McpSupport.tools(mcpConnectors, includeFunctions, excludeFunctions, attrs) ++ A2ASupport.tools(attrs) ++ SearchEngineSupport.tools(searchEngines)
     Json.obj(
       "tools" -> tools
     )
   }
 
   def tools(wasmFunctions: Seq[String], mcpConnectors: Seq[String], includeFunctions: Seq[String], excludeFunctions: Seq[String], attrs: TypedMap, searchEngines: Seq[String] = Seq.empty)(implicit ec: ExecutionContext, env: Env): JsObject = {
-    val tools: Seq[JsObject] = LlmToolFunction._tools(wasmFunctions) ++ McpSupport.tools(mcpConnectors, includeFunctions, excludeFunctions, attrs) ++ SearchEngineSupport.tools(searchEngines)
+    val tools: Seq[JsObject] = LlmToolFunction._tools(wasmFunctions) ++ McpSupport.tools(mcpConnectors, includeFunctions, excludeFunctions, attrs) ++ A2ASupport.tools(attrs) ++ SearchEngineSupport.tools(searchEngines)
     Json.obj(
       "tools" -> tools
     )
   }
 
   def toolsAnthropic(wasmFunctions: Seq[String], mcpConnectors: Seq[String], includeFunctions: Seq[String], excludeFunctions: Seq[String], attrs: TypedMap, searchEngines: Seq[String] = Seq.empty)(implicit ec: ExecutionContext, env: Env): JsObject = {
-    val tools: Seq[JsObject] = LlmToolFunction._toolsAnthropic(wasmFunctions) ++ McpSupport.toolsAnthropic(mcpConnectors, includeFunctions, excludeFunctions, attrs) ++ SearchEngineSupport.toolsAnthropic(searchEngines)
+    val tools: Seq[JsObject] = LlmToolFunction._toolsAnthropic(wasmFunctions) ++ McpSupport.toolsAnthropic(mcpConnectors, includeFunctions, excludeFunctions, attrs) ++ A2ASupport.toolsAnthropic(attrs) ++ SearchEngineSupport.toolsAnthropic(searchEngines)
     Json.obj(
       "tools" -> tools
     )
   }
 
   def toolsAnthropicWithInline(wasmFunctions: Seq[String], inlineFunctions: Seq[String], mcpConnectors: Seq[String], includeFunctions: Seq[String], excludeFunctions: Seq[String], attrs: TypedMap, searchEngines: Seq[String] = Seq.empty)(implicit ec: ExecutionContext, env: Env): JsObject = {
-    val tools: Seq[JsObject] = LlmToolFunction._toolsAnthropic(wasmFunctions) ++ LlmToolFunction._inlineToolsAnthropic(inlineFunctions, attrs) ++ McpSupport.toolsAnthropic(mcpConnectors, includeFunctions, excludeFunctions, attrs) ++ SearchEngineSupport.toolsAnthropic(searchEngines)
+    val tools: Seq[JsObject] = LlmToolFunction._toolsAnthropic(wasmFunctions) ++ LlmToolFunction._inlineToolsAnthropic(inlineFunctions, attrs) ++ McpSupport.toolsAnthropic(mcpConnectors, includeFunctions, excludeFunctions, attrs) ++ A2ASupport.toolsAnthropic(attrs) ++ SearchEngineSupport.toolsAnthropic(searchEngines)
     Json.obj(
       "tools" -> tools
     )
@@ -112,9 +124,10 @@ object LlmFunctions {
   def toolsCohere(wasmFunctions: Seq[String], mcpConnectors: Seq[String], includeFunctions: Seq[String], excludeFunctions: Seq[String], attrs: TypedMap, searchEngines: Seq[String] = Seq.empty)(implicit ec: ExecutionContext, env: Env): (JsObject, Map[String, String]) = {
     val (wasmTools, wasmMap) = LlmToolFunction.toolsCohere(wasmFunctions)
     val (mcpTools, mcpMap) =  McpSupport.toolsCohere(mcpConnectors, includeFunctions, excludeFunctions, attrs)
+    val (a2aTools, a2aMap) = A2ASupport.toolsCohere(attrs)
     val (searchTools, searchMap) = SearchEngineSupport.toolsCohere(searchEngines)
-    val tools: Seq[JsObject] = wasmTools ++ mcpTools ++ searchTools
-    val map = wasmMap ++ mcpMap ++ searchMap
+    val tools: Seq[JsObject] = wasmTools ++ mcpTools ++ a2aTools ++ searchTools
+    val map = wasmMap ++ mcpMap ++ a2aMap ++ searchMap
     (Json.obj(
       "tools" -> tools
     ), map)
@@ -123,10 +136,11 @@ object LlmFunctions {
   def toolsCohereWithInline(wasmFunctions: Seq[String], inlineFunctions: Seq[String], mcpConnectors: Seq[String], includeFunctions: Seq[String], excludeFunctions: Seq[String], attrs: TypedMap, searchEngines: Seq[String] = Seq.empty)(implicit ec: ExecutionContext, env: Env): (JsObject, Map[String, String]) = {
     val (wasmTools, wasmMap) = LlmToolFunction.toolsCohere(wasmFunctions)
     val (mcpTools, mcpMap) =  McpSupport.toolsCohere(mcpConnectors, includeFunctions, excludeFunctions, attrs)
+    val (a2aTools, a2aMap) = A2ASupport.toolsCohere(attrs)
     val (inlineTools, inlineMap) =  LlmToolFunction.inlineToolsCohere(inlineFunctions, attrs)
     val (searchTools, searchMap) = SearchEngineSupport.toolsCohere(searchEngines)
-    val tools: Seq[JsObject] = wasmTools ++ inlineTools ++ mcpTools ++ searchTools
-    val map = wasmMap ++ inlineMap ++ mcpMap ++ searchMap
+    val tools: Seq[JsObject] = wasmTools ++ inlineTools ++ mcpTools ++ a2aTools ++ searchTools
+    val map = wasmMap ++ inlineMap ++ mcpMap ++ a2aMap ++ searchMap
     (Json.obj(
       "tools" -> tools
     ), map)
